5c805563f7f4de9e0a81394541488b5617bdbb8c
FrancoCidDiaz <franco.giovanni.cid.diaz@gmail.com>
2023-07-13 18:06:19 -0400

    recarga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
-import './App.css';
+import './App.css'
+import { useEffect } from 'react';
 import AddExercise from './Components/AddExercise';
 import Header from './Components/Header';
 import InputList from './Components/InputList';
@@ -8,6 +9,20 @@ import TemporizadorSeries from './Components/TemporizadorSeries';
 
 
 function App() {
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = '';
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <div className="App">
   <Header/>
